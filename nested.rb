afa5e455a9a26839e6c727c8e28a36c8e723fa8b
Kane Koo <kanekoo@gmail.com>
2019-03-26 00:31:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -15,7 +16,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
+ #    binding.pry
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +39,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash.dig 
 end
 
 def adding_matz
